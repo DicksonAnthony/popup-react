eaa397afa2d5dc540dc644c62f65602faa8f5159
DicksonAnthony <huntaba6@gmail.com>
2022-07-01 21:40:30 +0100

    Done but not mobile responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import Modal from "./components/Modal";
+import { GlobalStyle } from "./GlobalStyle";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Button = styled.button`
+  min-width: 100px;
+  padding: 16px 32px;
+  border-radius: 4px;
+  border: none;
+  background: #7014ee;
+  color: #fff;
+  font-size: 24px;
+  cursor: pointer;
+`;
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal((prev) => !prev);
+  };
+
   return (
     <>
-    <h1>
-      h1
-    </h1>
+      <Container>
+        <Button onClick={openModal}>I'm a modal</Button>
+        <Modal showModal={showModal} setShowModal={setShowModal} />
+        <GlobalStyle />
+      </Container>
     </>
   );
 }
